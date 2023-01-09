87d4591b61b0527399c1e2cdb33ba3b1732836fb
Wpstorm <wpstormdev@gmail.com>
2023-01-09 14:34:14 +0330

    Now the user can register on the homepage

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'username' => 'required',
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        User::create($incomingFields);
+        return 'Hello from register function.';
+    }
+}
