e8bfbd931ca350822a682cdb83530af509136b84
Wpstorm <wpstormdev@gmail.com>
2023-01-09 15:06:54 +0330

    Added register form inputs validations.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
         $incomingFields = $request->validate([
-            'username' => 'required',
-            'email' => 'required',
-            'password' => 'required'
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'confirmed']
         ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+
         User::create($incomingFields);
         return 'Hello from register function.';
     }
