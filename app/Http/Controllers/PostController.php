d1ba7909231a391e916c0c7b9f2713d583db3646
Wpstorm <wpstormdev@gmail.com>
2023-01-11 00:23:21 +0330

    Create single-post page for posts, redirect user to new post page after creating a post and show flash message.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,8 +23,13 @@ class PostController extends Controller
         $incomingFields['body'] = strip_tags($incomingFields['body']);
         $incomingFields['user_id'] = auth()->id();
 
-        Post::create($incomingFields);
+        $newPost = Post::create($incomingFields);
 
-        return "hey!!!";
+        return redirect("/post/{$newPost->id}")->with('success', 'New post was successfully created.');
+    }
+
+    public function viewSinglePost(Post $post)
+    {
+        return view('single-post', ['post' => $post]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,4 @@ Route::post('/logout', [UserController::class, "logout"]);
 //Blog post related routes
 Route::get('/create-post', [PostController::class, "showCreateForm"]);
 Route::post('/create-post', [PostController::class, "storeNewPost"]);
+Route::get('/post/{post}', [PostController::class, "viewSinglePost"]);
