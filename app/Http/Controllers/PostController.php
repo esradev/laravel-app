1fcf917442fa89639fa956336bc5dcad8456030e
Wpstorm <wpstormdev@gmail.com>
2023-01-10 23:56:54 +0330

    Now the user can create post and submit it to DB posts table.

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function showCreateForm()
+    {
+        return view('create-post');
+    }
+
+    public function storeNewPost(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+        $incomingFields['user_id'] = auth()->id();
+
+        Post::create($incomingFields);
+
+        return "hey!!!";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 
@@ -15,10 +16,12 @@ use App\Http\Controllers\UserController;
 |
 */
 
+// User related routes
 Route::get('/', [UserController::class, "showCorrectHomepage"]);
-
 Route::post('/register', [UserController::class, "register"]);
-
 Route::post('/login', [UserController::class, "login"]);
-
 Route::post('/logout', [UserController::class, "logout"]);
+
+//Blog post related routes
+Route::get('/create-post', [PostController::class, "showCreateForm"]);
+Route::post('/create-post', [PostController::class, "storeNewPost"]);
